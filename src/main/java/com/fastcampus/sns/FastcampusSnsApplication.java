3889aa4335ebd974e757acf5abce493f6f7ce5e4
junyoungp <jyjyjy89@gmail.com>
2022-11-28 21:34:27 +0900

    initialize project

---
+++ src/main/java/com/fastcampus/sns/FastcampusSnsApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusSnsApplication.class, args);
+    }
+
+}
